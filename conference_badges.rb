f7526304afaa35265b03bac38b4cb2a6af7f0c92
Regina Furness <skritschy@gmail.com>
2020-04-15 01:06:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,11 +1,11 @@
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(names)
   badge_array=[]
   names.each do |badge| 
-  badge_array>>"Hello, my name is #{badge}"
+  badge_array>>"Hello, my name is #{badge}."
   end
   badge_array
 end
