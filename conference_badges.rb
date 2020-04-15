4d3c6ed1f2a0ed724e80a31088b94332052aad53
Regina Furness <skritschy@gmail.com>
2020-04-15 00:52:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,9 @@ def batch_badge_creator(names)
   names.each {|name| puts "Hello, my name is #{name}."
 end
 
-def assign_rooms(rooms)
-  
+def assign_rooms(speaker)
+  room = 1
+  speaker.each do
+    |name| "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
 end
